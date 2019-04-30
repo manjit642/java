80c4ef711035ac8f3be7ea464fd40726fb2c94af
manjit642 <manjit642@gmail.com>
2019-04-30 09:56:21 +0530

    helloworld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld{
 public static void main(String [] args)
 	{
 		int a =1200;
-	System.out.println(" First Java Code" +a);
+	System.out.println(" First Java Code first" +a);
 	}
 			}
